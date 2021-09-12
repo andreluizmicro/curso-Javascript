cfd26528c2a8fa5b2fa9066c3537fcb7fbdbcb80
André Luiz da Silva <andreluizmicro@gmail.com>
2021-09-12 09:09:01 -0300

    Adicionado arquivos finais do modulo de estudo de funçoes

---
+++ exercicios-js/03-funcao/18-funçãoFactory1.js
@@ -0,0 +1,11 @@
+//Função factory é uma função que retorna um objeto
+
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
---
+++ exercicios-js/03-funcao/22-IIFE.js
@@ -0,0 +1,11 @@
+// IIFE-> immediately Invoked Function Expression
+// Função auto invocada
+// É importante sempre fugir do escopo global
+
+// Já executa a função
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abragente!')
+})()
+
+
